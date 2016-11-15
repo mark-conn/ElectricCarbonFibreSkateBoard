3d39a6c82f21539440e4d65c184c2e20b4a624d5
Mat Kitching <gdsm.kitching@gmail.com>
2016-11-15 20:58:26 +0000

    Home component included

---
+++ src/js/components/Home.jsx
@@ -0,0 +1,25 @@
+var React = require('react');
+
+
+
+var Home = React.createClass({
+  // ...
+  showLock: function() {
+    // Show the Auth0Lock widget
+    this.props.lock.social({
+  connections: ["facebook", "google-oauth2"]
+});;
+  },
+
+  render: function() {
+    return (
+    <div className="login-box">
+      <button className ="login-button">
+        <a className="login" onClick={this.showLock}>Sign In</a>
+      </button>
+    </div>);
+  }
+});
+
+
+module.exports = Home;
