684a3546769647edb645e2f3cd64acb26021d91c
Mark Conn <misterconn@gmail.com>
2016-11-16 16:37:16 +0000

    combining react and server

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,39 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
+var ReactRouter = require('react-router');
+const Auth0Lock = require('auth0-lock-passwordless').default;
+
+var Router = ReactRouter.Router;
+var Route = ReactRouter.Route;
+var IndexRoute = ReactRouter.IndexRoute;
+
+var Login = require('./components/Login');
+var Home = require('./components/Home')
 
 var App = require('./components/App');
+var Landing = require('./components/Landing')
 
-ReactDOM.render(<App/>, document.getElementById('app'));
+
+/*
+Rendering a router will output the right component tree based on the current URL.
+Nested routes' components will be passed down to the parent as `this.props.children`
+
+If the URL is /, then <App/> will be rendered, and this.props.children will be <Search/> (this is the IndexRoute)
+If the URL is /user/ziad-saab then <App/> will be rendered, and this.props.children will be <User/>
+The <User/> instance will be passed a prop called `params`. It will be an object with `{username: 'ziad-saab'}`
+*/
+var routes = (
+    
+    <Router history={ReactRouter.browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Login}>
+            <Route path="/" component={Home}/>
+            </IndexRoute>
+            <Route path="homepage" component={Landing}/>
+        </Route>
+    </Router>
+);
+
+
+
+ReactDOM.render(routes, document.getElementById('app'));
---
+++ src/js/components/App.jsx
@@ -0,0 +1,32 @@
+var React = require('react');
+var Link = require('react-router').Link;
+import Auth0Lock from 'auth0-lock-passwordless';
+import Home from './Home';
+
+
+/*
+This is the layout component. It's displayed by the top-level Route
+this.props.children will correspond to the current URL's component.
+
+If the URL is only / then the IndexRoute's component will be the child (Search component)
+If the URL is /user/:username then the User component will be displayed.
+*/
+var App = React.createClass({
+
+    render: function() {
+        return (
+            <div className="main-app">
+                <header className="main-header">
+                    <h1><Link to="/">ECF Board</Link></h1>
+                    <img src="./battery_20_white_72x72.png"/>
+                </header>
+                <main className="main-content">
+                    {this.props.children}
+                </main>
+            </div>
+                
+        );
+    }
+});
+
+module.exports = App;
