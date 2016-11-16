87c20fd5b77ecc7f5d4cabc2c3699f70822d10e8
Mark Conn <misterconn@gmail.com>
2016-11-16 16:43:21 +0000

    updated src files

---
+++ src/js/components/Landing.jsx
@@ -0,0 +1,17 @@
+var React = require('react');
+
+
+var Landing = React.createClass({
+    
+    
+    
+  render(){
+    return (
+        <h1>Test</h1>
+        )    
+    }
+})
+
+
+
+module.exports = Landing
